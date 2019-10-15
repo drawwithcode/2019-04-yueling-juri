41e46c4e522c0454b5c1053e7d07d1ae0a310921
yueling-juri <55393310+yueling-juri@users.noreply.github.com>
2019-10-15 23:18:35 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,39 @@
-function preload(){
-  // put preload code here
+var gif_loadImg, gif_createImg;
+var bGm_1;
+var bGm_2;
+
+
+function preload() {
+  gif_loadImg = loadImage("./assets/source.gif");
+  gif_createImg = createImg("./assets/source.gif");
+  bGm_1 = createAudio("./assets/snow&wind_sound.wav");
+  bGm_2 = createAudio("./assets/snow_falling_sound.mp3");
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+
+  var myText = "touch top to open window";
+
+  textFont("Playfair Display");
+  textAlign(LEFT);
+  textSize(30);
+  fill("white");
+  text(myText, 10, height / 7 * 6);
+
 }
 
 function draw() {
-  // put drawing code here
+  gif_createImg.position(370, 100);
+  bGm_1.play();
+  bGm_2.play();
+
+  if (mouseY <= height / 2) {
+    bGm_1.volume(0.5 + (height / 2 - mouseY) / height);
+    bGm_2.volume(0.5 + (height / 2 - mouseY) / height);
+  } else {
+    bGm_1.volume(0.5 - (mouseY - height / 2) / height);
+    bGm_2.volume(0.5 - (mouseY - height / 2) / height);
+  }
 }
